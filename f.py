3a3fccf765d39c467b3de2fe597bdca251015604
kdh <@gmail.com>
2025-07-10 14:42:38 +0900

    파이썬 기초 실습

---
+++ f.py
@@ -0,0 +1,12 @@
+# 문자열 속애서 변수명을 넣고 싶으면 f"{변수} "
+#print(a)
+#print("결과는 a") ->a 변수 의미 ->오류->f"결과는 {a}"
+# product = "coffee"
+# count = 3
+# price= 10000
+# print(f"상품 {product} {count}개의 가격은 {count*price}원입니다.")
+
+s = "monty python"
+print(s[0]) #인덱싱():색인
+print(s[6:10]) #슬라이싱():범위추출
+#뒤숫자 -1=> 6~9
